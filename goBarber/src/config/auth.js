071a8b9ac354351486847951af3ab5cd8a2fd8f3
paulohenriquepm <paulinhodiv@hotmail.com>
2020-01-29 21:47:53 -0300

    Module 02 completed

---
+++ goBarber/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '7123d8e320bd030bec0737b8017ee962',
+  expiresIn: '7d',
+};
--- goBarber/src/routes.js
+++ goBarber/src/routes.js
@@ -1,17 +1,17 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Paulo Henrique',
-    email: 'paulo.henriquepm@outlook.com',
-    password_hash: '1234567890',
-  });
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
-  return res.json(user);
-});
+routes.use(authMiddleware); // so vale para as rotas posterioes
+
+routes.put('/users', UserController.update);
 
 export default routes;

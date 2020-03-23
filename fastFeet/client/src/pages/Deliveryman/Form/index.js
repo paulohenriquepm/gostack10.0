96d42324d579b4344846718411dd6b3560c533ed
paulohenriquepm <paulinhodiv@hotmail.com>
2020-03-23 11:26:42 -0300

    Starting deliveryman form

--- fastFeet/client/src/pages/Deliveryman/Form/index.js
+++ fastFeet/client/src/pages/Deliveryman/Form/index.js
@@ -6,10 +6,8 @@ import { toast } from 'react-toastify';
 import api from '~/services/api';
 import history from '~/services/history';
 
-import { FormContainer, FormLoading, Input, Select } from '~/components/Form';
+import { FormContainer, FormLoading, Input } from '~/components/Form';
 import { HeaderForm } from '~/components/ActionHeader';
-
-import { SelectContainer } from './styles';
 
 // const schema = Yup.object().shape({
 //   product: Yup.string().required('O producto da entrega é obrigatório'),
@@ -18,5 +16,57 @@ import { SelectContainer } from './styles';
 // });
 
 export default function DeliverymanForm({ match }) {
-  return <h1>Deliveryman Form</h1>;
+  const { id } = match.params;
+
+  const [loading, setLoading] = useState(false);
+  const [deliveryman, setDeliveryman] = useState([]);
+
+  useEffect(() => {
+    if (id) {
+      // eslint-disable-next-line no-inner-declarations
+      async function loadOrderDetails() {
+        try {
+          setLoading(true);
+
+          const response = await api.get(`deliverymans/${id}`);
+
+          setDeliveryman(response.data);
+
+          setLoading(false);
+        } catch (err) {
+          setLoading(false);
+          toast.error('Falha ao carregar dados');
+        }
+      }
+      loadOrderDetails();
+    }
+  }, [id]);
+
+  return (
+    <>
+      {loading ? (
+        <FormLoading />
+      ) : (
+        <FormContainer
+          initialData={deliveryman}
+          // onSubmit={handleSubmit}
+          // schema={schema}
+        >
+          <HeaderForm id={id} prevPage="/deliveymans" title="entregadores" />
+          <section>
+            <Input name="name" label="Nome" />
+            <Input name="email" label="Email" />
+          </section>
+        </FormContainer>
+      )}
+    </>
+  );
 }
+
+DeliverymanForm.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.node,
+    }).isRequired,
+  }).isRequired,
+};
--- fastFeet/client/src/pages/Deliveryman/Form/styles.js
+++ fastFeet/client/src/pages/Deliveryman/Form/styles.js
@@ -1,14 +1,1 @@
 import styled from 'styled-components';
-
-export const SelectContainer = styled.div`
-  display: flex;
-
-  > div {
-    flex: 1;
-    margin-bottom: 20px;
-
-    & + div {
-      margin-left: 30px;
-    }
-  }
-`;

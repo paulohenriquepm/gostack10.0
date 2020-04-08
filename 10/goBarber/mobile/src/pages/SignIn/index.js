9cc8b1c5255a9072b596cb0d2d99b22f532ad339
paulohenriquepm <paulinhodiv@hotmail.com>
2020-04-08 09:28:49 -0300

    new folder structure

---
+++ 10/goBarber/mobile/src/pages/SignIn/index.js
@@ -0,0 +1,70 @@
+import React, { useRef, useState } from 'react';
+import { Image, Alert } from 'react-native';
+import { useDispatch } from 'react-redux';
+
+import { singInRequest } from '~/store/modules/auth/actions';
+
+import logo from '~/assets/logo.png';
+
+import Background from '~/components/Background';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignIn({ navigation }) {
+  const dispatch = useDispatch();
+  const passwordRef = useRef();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit() {
+    Alert.alert('alert', 'alert');
+    // dispatch(singInRequest(email, password));
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
+          />
+
+          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+
+          <SignLink onPress={() => navigation.navigate('SignUp')}>
+            <SignLinkText>Criar conta gratuita</SignLinkText>
+          </SignLink>
+        </Form>
+      </Container>
+    </Background>
+  );
+}
---
+++ 10/goBarber/mobile/src/store/modules/auth/sagas.js
@@ -0,0 +1,83 @@
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { Alert } from 'react-native';
+
+// import history from '~/services/history';
+import api from '~/services/api';
+
+import { singInSuccess, signFailure } from './actions';
+
+export function* singIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    if (user.provider) {
+      Alert.alert(
+        'Falha na autenticação',
+        'O usuário não pode ser prestador de serviço'
+      );
+      return;
+    }
+
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
+    yield put(singInSuccess(token, user));
+
+    // history.push('/dashboard');
+  } catch (err) {
+    Alert.alert(
+      'Falha na autenticação',
+      'Houve um erro no login, verifique seus dados'
+    );
+    yield put(signFailure());
+  }
+}
+
+export function* singUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+    });
+
+    // history.push('/');
+  } catch (err) {
+    Alert.alert(
+      'Falha na autenticação',
+      'Houve um erro no cadastro, verifique seus dados'
+    );
+    yield put(signFailure());
+  }
+}
+
+function setToken({ payload }) {
+  if (!payload) {
+    return;
+  }
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export function signOut() {
+  // history.push('/');
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', singIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', singUp),
+  takeLatest('@auth/SIGN_OUT', signOut),
+]);
